58c2d478f5e32ec720a4288a68dd17b38f624ebd
marlonx19 <marlon.englemam@gmail.com>
2020-01-16 14:42:32 -0300

    ImagePicker added

--- src/screens/upload.js
+++ src/screens/upload.js
@@ -1,20 +1,66 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 import Header from './../components/header'
 
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
 export default function Upload(props) {
+  const [avatarSource, setAvatarSource] = useState('')
+
+  const launch = () => {
+    ImagePicker.launchImageLibrary({
+      mediaTypes: 'Images',
+      allowsEditing: true,
+      quality: 1
+    }, (response) => {
+      if (response.uri) {
+        setAvatarSource(response.uri)
+      }
+
+    });
+  }
+
+
+
+
   return (
     <View style={styles.container} >
-         <Header label='Upload' navigation={props.navigation} />
-        <Text>Upload goes here</Text>
+      <Header label='Upload' navigation={props.navigation} />
+      <View style={styles.main}>
+        <TouchableOpacity
+          onPress={() => launch()}
+        >
+          <Text>Upload goes here</Text>
+        </TouchableOpacity>
+        <Image source={{ uri: avatarSource }} style={styles.uploadAvatar} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
+  container: {
+    flex: 1
 
-    }
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  uploadAvatar: {
+    width: '100%',
+    height: 250
+  }
 })
