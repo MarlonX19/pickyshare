83e9cabbf0326f23f42f56e6bf9965d4aca86b27
marlonx19 <marlon.englemam@gmail.com>
2020-01-16 00:22:44 -0300

    Fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,25 @@ import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
 export default function Header(props) {
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={{ width: 100, flexDirection: 'row', alignItems: 'center' }} onPress={() => props.navigation.goBack()}>
-                <Image style={{ marginHorizontal: 10, width: 20, height: 20 }} source={require('../assets/back.png')} />
-            </TouchableOpacity>
+            {props.label == 'Feed' ? (
+                <>
+                    <TouchableOpacity
+                        onPress={() => props.navigation.navigate('Upload')}
+                    >
+                        <Image style={{ width: 30, height: 30, marginLeft: 10 }} source={require('../assets/camera2.png')} />
+                    </TouchableOpacity>
+                </>
+            ) : (
+                    <TouchableOpacity style={{ width: 100, flexDirection: 'row', alignItems: 'center' }} onPress={() => props.navigation.goBack()}>
+                        <Image style={{ marginHorizontal: 10, width: 20, height: 20 }} source={require('../assets/back.png')} />
+                    </TouchableOpacity>
+                )}
             <Text style={{ fontSize: 18 }}> {props.label} </Text>
-            <Text style={{ marginHorizontal: 10, fontSize: 18, textAlign: 'right', width: 100 }}>?</Text>
+            {props.label == 'Feed' ? (
+                <Image style={{ width: 30, height: 30, marginRight: 10 }} source={require('../assets/send.png')} />
+            ) : (
+                    <Text style={{ marginHorizontal: 10, fontSize: 18, textAlign: 'right', width: 100 }}>?</Text>
+                )}
         </View>
     );
 }
